a863cf34081084e21d4058916b4f8df29121ff65
dlagurgur <dlagrugru@naver.com>
2021-04-30 15:15:51 +0900

    12
    12

--- src/handler/Svc_pro.java
+++ src/handler/Svc_pro.java
@@ -420,12 +420,13 @@ public class Svc_pro{
 		product_dto.setUser_id(user_id);
 		product_dto.setProduct_image(filename);
 			
-		Product_Dao.insertProduct(product_dto);
-		int tb_no = product_dto.getProduct_id();// tb_no
-		request.setAttribute("tb_no", tb_no);	
+		
+	
 			
 		int result = Product_Dao.insertProduct(product_dto);
-
+		int tb_no = product_dto.getProduct_id();// tb_no
+		request.setAttribute("tb_no", tb_no);	
+		
 		request.setAttribute("result", result);
 		return new ModelAndView("svc/product_insert_pro");
 	}
