e40153b9f9123926fd356438c60da3e2e7da2f67
dlagurgur <43362557+dlagurgur@users.noreply.github.com>
2021-05-17 10:29:23 +0900

    sql
    sql

--- WebContent/newfile.sql
+++ WebContent/newfile.sql
@@ -19,8 +19,8 @@ CREATE TABLE Order_history
 	order_qnt int NOT NULL,
 	product_id int NOT NULL,
 	user_id varchar(20) NOT NULL,
-	PRIMARY KEY (order_num),
-	ON DELETE CASCADE
+	PRIMARY KEY (order_num)
+
 );
 
 
@@ -35,8 +35,8 @@ CREATE TABLE product
 	user_id varchar(20) NOT NULL,
 	aws_url varchar(200) NOT NULL,
 	PRIMARY KEY (product_id),
-	UNIQUE (product_id),
-	ON DELETE CASCADE
+	UNIQUE (product_id)
+
 );
 
 
@@ -51,8 +51,8 @@ CREATE TABLE trailer
 	trailer_detail varchar(5000) NOT NULL,
 	user_id varchar(20) NOT NULL,
 	PRIMARY KEY (trailer_id),
-	UNIQUE (trailer_id),
-	ON DELETE CASCADE
+	UNIQUE (trailer_id)
+
 );
 
 
@@ -75,121 +75,7 @@ CREATE TABLE User_general
 	c varchar(1000) NOT NULL,
 	PRIMARY KEY (user_id),
 	UNIQUE (user_id),
-	UNIQUE (user_email),
-	ON DELETE CASCADE
-);
-
-
-
-/* Create Foreign Keys */
-
-ALTER TABLE Order_history
-	ADD FOREIGN KEY (product_id)
-	REFERENCES product (product_id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE Order_history
-	ADD FOREIGN KEY (user_id)
-	REFERENCES User_general (user_id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE product
-	ADD FOREIGN KEY (user_id)
-	REFERENCES User_general (user_id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE trailer
-	ADD FOREIGN KEY (user_id)
-	REFERENCES User_general (user_id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-	SET SESSION FOREIGN_KEY_CHECKS=0;
-
-/* Drop Tables */
-
-DROP TABLE IF EXISTS Order_history;
-DROP TABLE IF EXISTS product;
-DROP TABLE IF EXISTS User_general;
-DROP TABLE IF EXISTS trailer;
-
-
-
-
-/* Create Tables */
-
-CREATE TABLE Order_history
-(
-	order_num int NOT NULL AUTO_INCREMENT,
-	order_date  timestamp,
-	order_qnt int NOT NULL,
-	product_id int NOT NULL,
-	user_id varchar(20) NOT NULL,
-	PRIMARY KEY (order_num),
-	ON DELETE CASCADE
-);
-
-
-CREATE TABLE product
-(
-	product_id int NOT NULL AUTO_INCREMENT,
-	product_name varchar(25) NOT NULL,
-	product_price int NOT NULL,
-	product_image varchar(300),
-	product_category int NOT NULL,
-	product_detail varchar(5000) NOT NULL,
-	user_id varchar(20) NOT NULL,
-	aws_url varchar(200) NOT NULL,
-	PRIMARY KEY (product_id),
-	UNIQUE (product_id),
-	ON DELETE CASCADE
-);
-
-
-
-CREATE TABLE trailer
-(
-	trailer_id int NOT NULL AUTO_INCREMENT,
-	trailer_name varchar(25) NOT NULL,
-	trailer_title varchar(25) NOT NULL,
-	trailer_price int NOT NULL,
-	trailer_aws_url varchar(300),
-	trailer_detail varchar(5000) NOT NULL,
-	user_id varchar(20) NOT NULL,
-	PRIMARY KEY (trailer_id),
-	UNIQUE (trailer_id),
-	ON DELETE CASCADE
-);
-
-
-
-
-
-
-CREATE TABLE User_general
-(
-	user_id varchar(20) NOT NULL,
-	user_pw varchar(20) NOT NULL,
-	user_name varchar(10) NOT NULL,
-	user_phone varchar(20) NOT NULL,
-	user_addr varchar(30),
-	user_addr2 varchar(20) NOT NULL,
-	user_email varchar(30) NOT NULL,
-	user_corp int(5) NOT NULL,
-	a varchar(1000) NOT NULL,
-	b varchar(1000) NOT NULL,
-	c varchar(1000) NOT NULL,
-	PRIMARY KEY (user_id),
-	UNIQUE (user_id),
-	UNIQUE (user_email),
+	UNIQUE (user_email)
 );
 
 
@@ -227,20 +113,6 @@ ALTER TABLE trailer
 	ON DELETE RESTRICT
 ;
 
-
-
-ALTER TABLE product convert to charset utf8;
-
-ALTER TABLE Order_history convert to charset utf8;
-
-ALTER TABLE User_general convert to charset utf8;
-
-ALTER TABLE trailer convert to charset utf8;
-
-
-
-
-;
 
 
 
